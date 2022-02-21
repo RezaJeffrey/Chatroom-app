aeae09235a74015bf196651ada7b018c39c8f168
Reza jeff <niceman.82@yahoo.com>
2022-02-21 13:36:45 +0330

    some changes

--- chatroom/room/models.py
+++ chatroom/room/models.py
@@ -1,3 +1,19 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class Message(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    body = models.CharField(max_length=200)
+    posted = models.TimeField(auto_now_add=True)
+
+
+class Room(models.Model):
+    room_name = models.CharField(max_length=60)
+    users = models.ManyToManyField(User)
+    created_date = models.DateField(auto_now_add=True)
+    slug = models.SlugField(max_length=60)
+
+    def __str__(self) :
+        return self.room_name
+
---
+++ chatroom/room/urls.py
@@ -0,0 +1,18 @@
+from django.urls import  path
+from . import views
+
+app_name = 'room'
+urlpatterns = [
+
+]
+
+
+
+
+
+
+
+
+
+
+
