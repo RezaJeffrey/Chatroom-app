3db58295e13f302ea1a87cda399651890511c57e
Reza jeff <niceman.82@yahoo.com>
2022-03-07 01:03:52 +0330

    Edit message and delete message is now available. admins of each room can delete messages but not edit. Also, you can edit your message or delete it. also, the creator of each room will set on admin of the room by now

--- chatroom/room/urls.py
+++ chatroom/room/urls.py
@@ -8,10 +8,12 @@ urlpatterns = [
     path('create-room/', views.CreateRoomView.as_view(), name='create_room'),
     path('create-private-room/', views.CreatePrivateRoomView.as_view(), name='create_private_room'),
     path('<int:room_id>/<slug:room_slug>/', views.RoomInsideView.as_view(), name='room_inside'),
-    path('<int:room_id>/', views.JoinRoomView.as_view(), name='join_room'),
     path('private/<int:room_id>/<slug:room_slug>/', views.PrivateRoomInsideView.as_view(), name='private_room_inside'),
-
+    path('<int:room_id>/', views.JoinRoomView.as_view(), name='join_room'),
+    path('delete_message/<int:message_id>/', views.DeleteMessageView.as_view(), name='delete_message'),
+    path('edit_message/<int:message_id>/', views.EditMessageView.as_view(), name='edit_message'),
 ]
+
 
 
 
